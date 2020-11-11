83f5bca35c2e522f18f01d4a01daec79fbfeb3cf
schto223 <52720175+schto223@users.noreply.github.com>
2020-11-11 17:36:57 +0100

    Add files via upload

--- src/tableaux.jl
+++ src/tableaux.jl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ################################################################################
 # Tableaux
 #
@@ -295,40 +294,3 @@ function is_semistandard(Tab::Tableau)
   end
   return true
 end
-=======
-################################################################################
-# Tableaux
-#
-# Copyright (C) 2020 Ulrich Thiel, ulthiel.com/math
-################################################################################
-
-export Tableau, shape
-
-struct Tableau{T} <: AbstractArray{AbstractArray{T,1},1}
-   t::Array{Array{T,1},1}
-end
-
-function Base.show(io::IO, ::MIME"text/plain", Tab::Tableau)
-  print(io, Tab.t)
-end
-
-function Base.size(Tab::Tableau)
-  return size(Tab.t)
-end
-
-function Base.length(Tab::Tableau)
-  return length(Tab.t)
-end
-
-function Base.getindex(Tab::Tableau, i::Int)
-  return getindex(Tab.t,i)
-end
-
-function Base.getindex(Tab::Tableau, I::Vararg{Int, 2})
-  return getindex(getindex(Tab.t,I[1]), I[2])
-end
-
-function shape(Tab::Tableau{T}) where T
-  return Partition{T}([ length(Tab[i]) for i=1:length(Tab) ])
-end
->>>>>>> f57ae1262e7b820dd63640ed6a0b261f1e65cf7e
