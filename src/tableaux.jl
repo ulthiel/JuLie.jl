465ebf8b944f7df9778fd780366c0415b09b5eb0
schto223 <schto223@gmail.com>
2020-12-01 23:37:44 +0100

    ---Partitions.jl--- -added setindex! for Partitions since this is usefull when using Partitions to work with Tableaux.
    -added dominates(lambda,mu) which defines an ordering over Partitions of 
    the same number
    
    ---runtests.jl---
    -added tests for semistandard_tableaux and standard_tableaux
    
    ---tableaux.jl---
    -replaced all instances of deepcopy by a much more efficient code
    
    -added:
    semistandard_tableaux(s,weight)
    standard_tableaux(s)
    standard_tableaux(n)
    hook_length(Tab,i,j)
    hook_length(s,i,j)
    hook_length_formula(s)
    schur_polynomial(shp)
    weight(Tab)
    reading_word(Tab)
    charge(Tab)
    charge(word)
    kostka_polynomial(lambda,mu)
    
    -reordered a few functions in tableaux.jl

--- src/tableaux.jl
+++ src/tableaux.jl
@@ -4,7 +4,10 @@
 # Copyright (C) 2020 Ulrich Thiel, ulthiel.com/math
 ################################################################################
 
-export Tableau, shape, semistandard_tableaux, is_standard, is_semistandard, schensted
+import AbstractAlgebra: PolynomialRing, push_term!, MPolyBuildCtx, finish
+import Nemo: ZZ, QQ, fmpz, fmpz_mpoly, fmpz_poly, addeq!, mul!, div , libflint
+
+export Tableau, shape, semistandard_tableaux, is_standard, is_semistandard, schensted, hook_length, hook_length_formula, standard_tableaux, schur_polynomial, kostka_polynomial,reading_word, weight, charge
 
 struct Tableau{T} <: AbstractArray{AbstractArray{T,1},1}
    t::Array{Array{T,1},1}
@@ -63,7 +66,7 @@ function semistandard_tableaux(shape::Array{T,1}, max_val=sum(shape)::Integer) w
   n = shape[m]
 
   while true
-    push!(SST, Tableau{T}(deepcopy(Tab)))
+    push!(SST,Tableau([copy(row) for row in Tab]))
 
     #raise one element by 1
     while !(Tab[m][n]<max_val &&
@@ -127,11 +130,298 @@ function semistandard_tableaux(box_num::T, max_val=box_num::T) where T<:Integer
   return SST
 end
 
+
+"""
+   semistandard_tableaux(s::Array{T,1}, weight::Array{T,1}) where T<:Integer
+
+returns an Array containing all semistandard tableaux with shape s and weights weight.
+
+requires that sum(s) == sum(weight)
+"""
+function semistandard_tableaux(s::Array{T,1}, weight::Array{T,1}) where T<:Integer
+  n_max = sum(s)
+  n_max==sum(weight) || throw(ArgumentError("sum(s) == sum(weight) required"))
+
+  Tabs = Array{Tableau,1}()
+  if isempty(s)
+    push!(Tabs, Tableau([[]]))
+    return Tabs
+  end
+  ls = length(s)
+
+  Tab = Tableau([ [0 for j = 1:s[i]] for i = 1:length(s)])
+  sub_s = zeros(Integer, length(s))
+
+  #tracker_row = zeros(Integer,n_max)
+
+  function rec_sst!(n::Integer)
+
+    #fill the remaining boxes if possible, else set them to 0
+    if n == length(weight)
+      for i = 1:ls
+        for j = sub_s[i]+1:s[i]
+          Tab[i][j] = n
+          if i!=1 && Tab[i-1][j]==n
+            for k = 1:i
+              for l = sub_s[k]+1:s[k]
+                Tab[i][j] = 0
+              end
+            end
+            return
+          end
+        end
+      end
+      push!(Tabs,Tableau([copy(row) for row in Tab]))
+
+      return
+    #skip to next step if weight[n]==0
+    elseif weight[n] == 0
+      rec_sst!(n+1)
+      return
+    end
+    #here starts the main part of the function
+    tracker_row = zeros(Integer, weight[n])
+    i = 1
+    while sub_s[i] == s[i]
+      i += 1
+    end
+    j = sub_s[i] + 1
+
+    m=0
+    while m >= 0
+      if m == weight[n]         #jump to next recursive step
+        rec_sst!(n+1)
+        Tab[tracker_row[m]][sub_s[tracker_row[m]]] = 0
+        i = tracker_row[m] + 1
+        if i <= ls
+          j = sub_s[i] + 1
+        end
+        m -= 1
+        sub_s[i-1] -= 1
+
+      elseif i > ls
+        if m == 0
+          return
+        else
+          Tab[tracker_row[m]][sub_s[tracker_row[m]]] = 0
+          i = tracker_row[m] + 1
+          if i <= ls
+            j = sub_s[i] + 1
+          end
+          m -= 1
+          sub_s[i-1] -= 1
+        end
+
+      elseif j<=s[i] && (i==1 || (j<=sub_s[i-1] && n>Tab[i-1][j]))  #add an entry
+        m += 1
+        Tab[i][j] = n
+        sub_s[i] += 1
+        tracker_row[m] = i
+        j += 1
+
+      else              #move pointerhead
+        i += 1
+        if i <= ls
+          j = sub_s[i] + 1
+        end
+      end
+    end  #while
+  end  #rec_sst!()
+
+  rec_sst!(1)
+  return Tabs
+end
+
+
+"""
+   semistandard_tableaux(s::Partition{T}, weight::Partition{T}) where T<:Integer
+
+returns an Array containing all semistandard tableaux with shape s and weights weight.
+
+requires that sum(s) == sum(weight)
+"""
+function semistandard_tableaux(s::Partition{T}, weight::Partition{T}) where T<:Integer
+  return semistandard_tableaux(Array{T,1}(s),Array{T,1}(weight))
+end
+
+
+"""
+    standard_tableaux(s::Array{Integer,1})
+
+returns a list of all standard young-Tableaux of a given shape s
+"""
+function standard_tableaux(s::Array{T,1}) where T<:Integer
+  return standard_tableaux(Partition(s))
+end
+
+
+"""
+    standard_tableaux(s::Partition)
+
+returns a list of all standard young-Tableaux of a given shape s
+"""
+function standard_tableaux(s::Partition)
+  Tabs = Array{Tableau,1}()
+  if isempty(s)
+    push!(Tabs, Tableau([[]]))
+    return Tabs
+  end
+  n_max = sum(s)
+  ls = length(s)
+
+  Tab = Tableau([ [0 for j = 1:s[i]] for i = 1:length(s)])
+  sub_s = [0 for i=1:length(s)]
+  Tab[1][1] = 1
+  sub_s[1] = 1
+  tracker_row = [0 for i=1:n_max]
+  tracker_row[1] = 1
+
+  n = 1
+  i = 1
+  j = 2
+
+  while n > 0
+    if n == n_max || i > ls
+      if n == n_max
+        push!(Tabs,Tableau([copy(row) for row in Tab]))
+      end
+      Tab[tracker_row[n]][sub_s[tracker_row[n]]] = 0
+      i = tracker_row[n] + 1
+      if i <= ls
+        j = sub_s[i] + 1
+      end
+      n -= 1
+      sub_s[i-1] -= 1
+    elseif j<=s[i] && (i==1 || j<=sub_s[i-1])
+      n += 1
+      Tab[i][j] = n
+      sub_s[i] += 1
+      tracker_row[n] = i
+      i = 1
+      j = sub_s[1] + 1
+    else
+      i += 1
+      if i <= ls
+        j = sub_s[i] + 1
+      end
+    end
+  end
+
+  return Tabs
+end
+
+
+"""
+    standard_tableaux(n::Integer)
+
+returns a list of all standard young-Tableaux of size n
+"""
+function standard_tableaux(n::Integer)
+  ST = Array{Tableau,1}()
+  for s in partitions(n)
+    append!(ST, standard_tableaux(s))
+  end
+  return ST
+end
+
+
+"""
+    is_standard(Tab::Tableau)
+
+Checks if Tab is a standard Tableau. i.e. a Tableau with strictly increasing columns and rows, where each number from 1 to n appears exactly once.
+"""
+function is_standard(Tab::Tableau)
+  #correct shape
+  s = shape(Tab)
+  for i = 1:length(s)-1
+    if s[i] < s[i+1]
+      return false
+    end
+  end
+
+  #contains all numbers from 1 to n
+  numbs = falses(sum(s))
+  for i = 1:length(s)
+    for j = 1:s[i]
+      numbs[Tab[i][j]] = true
+    end
+  end
+  if false in numbs
+    return false
+  end
+
+  #increasing first row
+  for j = 2:length(s[1])
+    if Tab[1][j] <= Tab[1][j-1]
+      return false
+    end
+  end
+
+  #increasing first column
+  for i = 2:length(s)
+    if Tab[i][1] <= Tab[i-1][1]
+      return false
+    end
+  end
+
+  #increasing rows and columns
+  for i = 2:length(Tab)
+    for j = 2:length(Tab[i])
+      if Tab[i][j] <= Tab[i][j-1] || Tab[i][j] <= Tab[i-1][j]
+        return false
+      end
+    end
+  end
+  return true
+end
+
+
+"""
+    is_semistandard(Tab::Tableau)
+
+Checks if Tab is a semistandard Tableau. i.e. a Tableau with non decresing rows and strictly increasing columns.
+"""
+function is_semistandard(Tab::Tableau)
+  #correct shape
+  s = shape(Tab)
+  for i = 1:length(s)-1
+    if s[i] < s[i+1]
+      return false
+    end
+  end
+
+  #increasing first row
+  for j = 2:length(s[1])
+    if Tab[i][j] < Tab[i][j-1]
+      return false
+    end
+  end
+
+  #increasing first column
+  for i = 2:length(s)
+    if Tab[i][1] <= Tab[i-1][1]
+      return false
+    end
+  end
+
+  #increasing rows and columns
+  for i = 2:length(Tab)
+    for j = 2:length(Tab[i])
+      if Tab[i][j] < Tab[i][j-1] || Tab[i][j] <= Tab[i-1][j]
+        return false
+      end
+    end
+  end
+  return true
+end
+
+
 """
     schensted(sigma::Array{Integer,1})
 
 An implementation of the Schensted algorithm from the Robinson-Schensted correspondence.
-sigma represents the second line of a Permutation in two-line notation: 1->sigma[1] , 2->sigma[2] , ...
+sigma represents the second line of a Permutation in two-line notation:
+1->sigma[1] , 2->sigma[2] , ...
 Returns a pair of standard Tableaux P,Q (insertion- and recording- tableaux)
 """
 function schensted(sigma::Array{T,1}) where T<:Integer
@@ -206,91 +496,336 @@ end
 
 
 """
-    is_standard(Tab::Tableau)
+    hook_length(Tab::Tableau, i::Integer, j::Integer)
 
-Checks if Tab is a standard Tableau. i.e. a Tableau with strictly increasing columns and rows, where each number from 1 to n appears exactly once.
+returns the hook length of Tab[i][j].
+assumes that Tab[i][j] exists
 """
-function is_standard(Tab::Tableau)
-  #correct shape
+function hook_length(Tab::Tableau, i::Integer, j::Integer)
   s = shape(Tab)
-  for i = 1:length(s)-1
-    if s[i] < s[i+1]
-      return false
-    end
+  h = s[i] - j + 1
+  k = j + 1
+  while k<=length(s) && s[k]>=j
+    k += 1
+    h += 1
   end
-
-  #contains all numbers from 1 to n
-  numbs = Array{Bool,1}(false,sum(s))
-  for i = 1:length(s)
-    for j = 1:s[i]
-      numbs[Tab[i][j]] = true
-    end
-  end
-  if false in numbs
-    return false
-  end
-
-  #increasing first row
-  for j = 2:length(s[1])
-    if Tab[1][j] <= Tab[1][j-1]
-      return false
-    end
-  end
-
-  #increasing first column
-  for i = 2:length(s)
-    if Tab[i][1] <= Tab[i-1][1]
-      return false
-    end
-  end
-
-  #increasing rows and columns
-  for i = 2:length(Tab)
-    for j = 2:length(Tab[i])
-      if Tab[i][j] <= Tab[i][j-1] || Tab[i][j] <= Tab[i-1][j]
-        return false
-      end
-    end
-  end
-  return true
+  return h
 end
 
 
 """
-    is_semistandard(Tab::Tableau)
+    hook_length(s::Partition, i::Integer, j::Integer)
 
-Checks if Tab is a semistandard Tableau. i.e. a Tableau with non decresing rows and strictly increasing columns.
+returns the hook length of Tab[i][j] for a Tableau Tab of shape s.
+assumes that i<=length(s) and j<=s[i]
 """
-function is_semistandard(Tab::Tableau)
-  #correct shape
-  s = shape(Tab)
-  for i = 1:length(s)-1
-    if s[i] < s[i+1]
-      return false
+function hook_length(s::Partition, i::Integer, j::Integer)
+  h = s[i] - j + 1
+  k = i + 1
+  while k<=length(s) && s[k]>=j
+    k += 1
+    h += 1
+  end
+  return h
+end
+
+
+"""
+    hook_length_formula(s::Partition)
+
+returns the hook length formula for a tableau of shape s.
+Equals the number of standard tableaux of shape s
+"""
+function hook_length_formula(s::Partition)
+  h=factorial(big(sum(s)))
+  for i = 1:length(s)
+    for j = 1:s[i]
+      h=Integer(h/hook_length(s,i,j))
     end
   end
+  return h
+end
 
-  #increasing first row
-  for j = 2:length(s[1])
-    if Tab[i][j] < Tab[i][j-1]
-      return false
-    end
-  end
+"""
+    schur_polynomial(shp::Partition)
 
-  #increasing first column
-  for i = 2:length(s)
-    if Tab[i][1] <= Tab[i-1][1]
-      return false
-    end
-  end
+returns the Schur function s_shp as a Multivariate Polynomial.
+s_shp=sum_{SSYT of shape shp} x_1^{m1}...x_n^{m_n}
+"""
+function schur_polynomial(shp::Partition)
+  num_boxes = sum(shp)
+  x = [string("x",string(i)) for i=1:num_boxes]
+  R,x = PolynomialRing(ZZ, x)
 
-  #increasing rows and columns
-  for i = 2:length(Tab)
-    for j = 2:length(Tab[i])
-      if Tab[i][j] < Tab[i][j-1] || Tab[i][j] <= Tab[i-1][j]
-        return false
+  sf = MPolyBuildCtx(R)
+
+  #version of the function semistandard_tableaux(shape::Array{T,1}, max_val=sum(shape)::Integer)
+  len = length(shp)
+  Tab = [(fill(i,shp[i])) for i = 1:len]
+  m = len
+  n = shp[m]
+
+  count = zeros(Int, num_boxes)
+
+  while true
+    count .= 0
+    for i = 1:len
+      for j = 1:shp[i]
+        count[Tab[i][j]] += 1
       end
     end
+    push_term!(sf, ZZ(1), count)
+
+    #raise one element by 1
+    while !(Tab[m][n] < num_boxes &&
+      (n==shp[m] || Tab[m][n]<Tab[m][n+1]) &&
+      (m==len || shp[m+1]<n || Tab[m][n]+1<Tab[m+1][n]))
+      if n > 1
+        n -= 1
+      elseif m > 1
+        m -= 1
+        n = shp[m]
+      else
+        return finish(sf)
+      end
+    end
+
+    Tab[m][n] += 1
+
+    #minimize trailing elemnts
+    if n < shp[m]
+      i = m
+      j = n + 1
+    else
+      i = m + 1
+      j = 1
+    end
+    while (i<=len && j<=shp[i])
+      if i == 1
+        Tab[1][j] = Tab[1][j-1]
+      elseif j == 1
+        Tab[i][1] = Tab[i-1][1] + 1
+      else
+        Tab[i][j] = max(Tab[i][j-1], Tab[i-1][j] + 1)
+      end
+      if j < shp[i]
+        j += 1
+      else
+        j = 1
+        i += 1
+      end
+    end
+    m = len
+    n = shp[len]
+  end #while
+end
+
+
+"""
+    weight(Tab::Tableau)
+
+returns the weight of the tableau Tab.
+i.e. w::Array{Int,1} such that w[i] = number of appearances of i in Tab
+"""
+function weight(Tab::Tableau)
+  max=0
+  for i=1:length(Tab)
+    if max<Tab[i][end]
+      max=Tab[i][end]
+    end
   end
-  return true
+
+  w = zeros(Int,max)
+  for rows in Tab
+    for box in rows
+      w[box] += 1
+    end
+  end
+  return w
+end
+
+function reading_word(Tab::Tableau)
+  w=zeros(Int,sum(shape(Tab)))
+  k=0
+  for i = length(Tab):-1:1
+    for j = 1:length(Tab[i])
+      k += 1
+      w[k] = Tab[i,j]
+    end
+  end
+  return w
+end
+
+"""
+    charge(Tab::Tableau)
+
+returns the charge of Tab.
+"""
+function charge(Tab::Tableau)
+  return charge(reading_word(Tab))
+end
+
+
+"""
+    charge(word::Array{Int,1},standard=false::Bool)
+
+returns the charge of the Tableau corresponding to the reading word: word
+"""
+function charge(word::Array{Int,1},standard=false::Bool)
+  c = 0
+  if !standard
+    baseword = copy(word)
+    while length(baseword) > 0
+      indices = Array{Int,1}()
+      a = 1
+      maxim = maximum(baseword)
+      while a <= maxim
+        emptyrun = true
+        for i = length(baseword):-1:1
+          if baseword[i] == a
+            push!(indices,i)
+            a += 1
+            emptyrun = false
+          end
+        end
+        if emptyrun
+          a += 1
+        end
+      end
+      sort!(indices)
+      w = splice!(baseword,indices)
+      c += charge(w,true)
+    end
+  else
+    index = 0
+    pointer = argmin(word)
+    maxim = maximum(word)
+    while word[pointer]!=maxim
+      nextmin = word[pointer]+1
+      while findfirst(==(nextmin),word) == nothing
+        nextmin += 1
+      end
+      if findfirst(==(nextmin),word) > pointer
+        index += 1
+      end
+      c += index
+      pointer = findfirst(==(nextmin),word)
+    end
+  end
+  return c
+
+end
+
+
+"""
+    kostka_polynomial(lambda::Partition, mu::Partition)
+
+returns the Kostka polynomial K_{lambda,mu} as a fmpz_poly over ZZ in t.
+"""
+function kostka_polynomial(lambda::Partition, mu::Partition)
+  R,t = PolynomialRing(ZZ, "t")
+  kos_poly = R()
+
+
+  s = Array{Int,1}(lambda)
+  weight = Array{Int,1}(mu)
+
+  #= this is almost the same Algorithm used in semistandard_tableaux(s,weight)
+  It is basically the following Algorithm:
+
+  for Tab in semistandard_tableaux(lambda,mu)
+    addeq!(kos_poly,t^charge(Tab))
+  end
+
+  =#
+  n_max = sum(s)
+  n_max==sum(weight) || throw(ArgumentError("sum(s) == sum(weight) required"))
+
+  if isempty(s)
+    return kos_poly
+  end
+  ls = length(s)
+
+  Tab = Tableau([ [0 for j = 1:s[i]] for i = 1:length(s)])
+  sub_s = zeros(Integer, length(s))
+
+  #tracker_row = zeros(Integer,n_max)
+
+  function rec_sst!(n::Integer)
+
+    #fill the remaining boxes if possible, else set them to 0
+    if n == length(weight)
+      for i = 1:ls
+        for j = sub_s[i]+1:s[i]
+          Tab[i][j] = n
+          if i!=1 && Tab[i-1][j]==n
+            for k = 1:i
+              for l = sub_s[k]+1:s[k]
+                Tab[i][j] = 0
+              end
+            end
+            return
+          end
+        end
+      end
+      addeq!(kos_poly,t^charge(Tab))    #this line is different from semistandard_tableaux(s,weight) - Algorithm
+      return
+    #skip to next step if weight[n]==0
+    elseif weight[n] == 0
+      rec_sst!(n+1)
+      return
+    end
+    #here starts the main part of the function
+    tracker_row = zeros(Integer, weight[n])
+    i = 1
+    while sub_s[i] == s[i]
+      i += 1
+    end
+    j = sub_s[i] + 1
+
+    m=0
+    while m >= 0
+      if m == weight[n]         #jump to next recursive step
+        rec_sst!(n+1)
+        Tab[tracker_row[m]][sub_s[tracker_row[m]]] = 0
+        i = tracker_row[m] + 1
+        if i <= ls
+          j = sub_s[i] + 1
+        end
+        m -= 1
+        sub_s[i-1] -= 1
+
+      elseif i > ls
+        if m == 0
+          return
+        else
+          Tab[tracker_row[m]][sub_s[tracker_row[m]]] = 0
+          i = tracker_row[m] + 1
+          if i <= ls
+            j = sub_s[i] + 1
+          end
+          m -= 1
+          sub_s[i-1] -= 1
+        end
+
+      elseif j<=s[i] && (i==1 || (j<=sub_s[i-1] && n>Tab[i-1][j]))  #add an entry
+        m += 1
+        Tab[i][j] = n
+        sub_s[i] += 1
+        tracker_row[m] = i
+        j += 1
+
+      else              #move pointerhead
+        i += 1
+        if i <= ls
+          j = sub_s[i] + 1
+        end
+      end
+    end  #while
+  end  #rec_sst!()
+
+  rec_sst!(1)
+
+  return kos_poly
+
 end
