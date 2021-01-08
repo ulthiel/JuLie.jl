83a500eb88db780b15c24dd1e79df9a58edfff2d
schto223 <52720175+schto223@users.noreply.github.com>
2021-01-08 15:50:28 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,23 @@ using Documenter, JuLie
 
 #makedocs(sitename = "JuLie Documentation", modules = [JuLie])
 
-makedocs(
-    modules = [JuLie],
-    format = Documenter.HTML(
-        # Use clean URLs, unless built as a "local" build
-        prettyurls = !("local" in ARGS),
-        canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
-    ),
-    sitename = "JuLie.jl",
-    authors = "Ulrich Thiel",
-    pages = [
-        "About" => "index.md",
-        "Combinatorics" => "combinatorics.md",
-        "Lie theory" => "lie-theory.md"
-    ]
-)
+#makedocs(
+#    modules = [JuLie],
+#    format = Documenter.HTML(
+#        # Use clean URLs, unless built as a "local" build
+#        prettyurls = !("local" in ARGS),
+#        canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
+#    ),
+#    sitename = "JuLie.jl",
+#    authors = "Ulrich Thiel",
+#    pages = [
+#        "About" => "index.md",
+#        "Combinatorics" => "combinatorics.md",
+#        "Lie theory" => "lie-theory.md"
+#    ]
+#)
+
+makedocs(sitename = "JuLie Documentation", modules = [JuLie])
 
 deploydocs(
     repo = "github.com/schto223/JuLie.jl.git",
