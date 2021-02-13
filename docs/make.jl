060ad27bb9ebc6b1df47ecad40251f23e2da064d
Ulrich Thiel <thiel@mathematik.uni-kl.de>
2021-02-13 10:17:25 +0100

    Some changes in make.jl for documentation.

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,25 @@
 using Documenter, JuLie
 
-#makedocs(sitename = "JuLie Documentation", modules = [JuLie])
-
 DocMeta.setdocmeta!(JuLie, :DocTestSetup, :(using JuLie); recursive = true)
 
 makedocs(
-    modules = [JuLie],
-    format = Documenter.HTML(
-        # Use clean URLs, unless built as a "local" build
-        prettyurls = !("local" in ARGS),
-        #canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
-    ),
-    sitename = "JuLie Documentation",
-#    authors = "Ulrich Thiel",
-    pages = [
-        "About" => "index.md",
-        "Combinatorics" => "combinatorics.md",
-        "Lie theory" => "lie-theory.md",
-        "Benchmarks" => "benchmarks.md",
-        "Index" => "julie-index.md"
-    ]
+	modules = [JuLie],
+	format = Documenter.HTML(
+		# Use clean URLs, unless built as a "local" build
+		prettyurls = !("local" in ARGS),
+		#canonical = "https://juliadocs.github.io/Documenter.jl/stable/",
+	),
+	sitename = "JuLie",
+	authors = "Ulrich Thiel",
+	pages = [
+		"About" => "index.md",
+		"Combinatorics" => "combinatorics.md",
+		"Lie theory" => "lie-theory.md",
+		"Benchmarks" => "benchmarks.md",
+		"Index" => "julie-index.md"
+	]
 )
 
 deploydocs(
-    repo = "github.com/ulthiel/JuLie.jl.git",
+	repo = "github.com/ulthiel/JuLie.jl.git",
 )
